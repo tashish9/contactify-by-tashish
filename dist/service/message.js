c7db963fc39743922ef4eee935f89a760a7ad9b0
Tashish Soni <tashishsoni12@gmail.com>
2022-09-26 03:47:10 +0530

    fixed otp func

--- dist/service/message.js
+++ dist/service/message.js
@@ -3,12 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendMessage = void 0;
 const constants_1 = require("../config/constants");
 const twilio_1 = require("twilio");
-const general_1 = require("./general");
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, FROM_NUMBER } = constants_1.CONSTANTS;
 const client = new twilio_1.Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 const sendMessage = async (data) => {
-    const OTP = (0, general_1.generateOTP)();
-    const OTPMessage = `Hi. Your OTP is ${OTP}`;
+    const OTPMessage = `Hi. Your OTP is ${data.otp}`;
     await client.messages.create({
         from: FROM_NUMBER,
         to: data.to,
--- src/service/message.ts
+++ src/service/message.ts
@@ -1,14 +1,12 @@
 import { CONSTANTS } from "../config/constants";
 import { Twilio } from "twilio";
 import { MessageData } from "@/models/message";
-import { generateOTP } from "./general";
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, FROM_NUMBER } = CONSTANTS;
 
 const client = new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 export const sendMessage = async (data: MessageData) => {
-  const OTP = generateOTP();
-  const OTPMessage = `Hi. Your OTP is ${OTP}`;
+  const OTPMessage = `Hi. Your OTP is ${data.otp}`;
 
   await client.messages.create({
     from: FROM_NUMBER,
