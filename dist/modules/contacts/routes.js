e91c89ab9de079cacf8c1ce44321a4e60a9708c7
Tashish Soni <tashishsoni12@gmail.com>
2022-09-26 03:42:25 +0530

    changed routes to /api

--- dist/modules/contacts/routes.js
+++ dist/modules/contacts/routes.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.initializeContactsRoutes = void 0;
 const controllers_1 = require("./controllers");
 const initializeContactsRoutes = (router) => {
-    router.get("/contacts", controllers_1.getContacts);
-    router.post("/send-message", controllers_1.sendMessageToContact);
+    router.get("/api/contacts", controllers_1.getContacts);
+    router.post("/api/send-message", controllers_1.sendMessageToContact);
 };
 exports.initializeContactsRoutes = initializeContactsRoutes;
 //# sourceMappingURL=routes.js.map
